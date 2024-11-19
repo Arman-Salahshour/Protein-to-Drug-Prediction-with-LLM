57dd94ca19842189474a798eedc40ae5fdcf928a
Arman Salahshour <salahshour_a@yahoo.com>
2024-11-19 15:56:31 +0330

    Add constants for hyperparameters and model configurations

---
+++ constants.py
@@ -0,0 +1,57 @@
+"""
+In llama_constants.py, all necessary packages for various code snippets are imported. 
+Additionally, the Llama tokenizer and model are defined in this file.
+"""
+
+import torch
+import warnings
+from unsloth import FastLanguageModel
+warnings.filterwarnings('ignore')
+
+'''Chose a gpu'''
+device = torch.device('cuda')
+print(device, flush=True)
+
+'''Constant hyperparameters'''
+max_seq_length = 2048
+dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+
+
+'''In the following, all models which are available by unsloth package are listed'''
+# 4bit pre quantized models we support for 4x faster downloading + no OOMs.
+fourbit_models = [
+    "unsloth/mistral-7b-v0.3-bnb-4bit",      # New Mistral v3 2x faster!
+    "unsloth/mistral-7b-instruct-v0.3-bnb-4bit",
+    "unsloth/llama-3-8b-bnb-4bit",           # Llama-3 15 trillion tokens model 2x faster!
+    "unsloth/llama-3-8b-Instruct-bnb-4bit",
+    "unsloth/llama-3-70b-bnb-4bit",
+    "unsloth/Phi-3-mini-4k-instruct",        # Phi-3 2x faster!
+    "unsloth/Phi-3-medium-4k-instruct",
+    "unsloth/mistral-7b-bnb-4bit",
+    "unsloth/gemma-7b-bnb-4bit",             # Gemma 2.2x faster!
+]
+
+model_label = "unsloth/Meta-Llama-3.1-8B"
+# model_name = model_label.split('/')[1].split('-')[0]
+model_name = 'Lama3.1'
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = model_label,
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit,
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 64, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 16,
+    lora_dropout = 0, # Supports any, but = 0 is optimized
+    bias = "none",    # Supports any, but = "none" is optimized
+    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+    random_state = 3407,
+    use_rslora = False,  # We support rank stabilized LoRA
+    loftq_config = None, # And LoftQ
+)
